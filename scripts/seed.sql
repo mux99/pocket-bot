83e4b8033354aac8a37894d5b2e95122736cfdd2
karitchi <karichikarichikarichi@gmail.com>
2023-10-15 12:52:29 +0000

    Refactor database schema and seed script
    - Introduce a new schema.sql file to define tables for users and sessions with improved naming.
    - Include execution instructions for executing the scripts using psql.
    - Add a seed.sql file to add a user to the database.
    - Remove the outdated tables.sql file.

---
+++ scripts/seed.sql
@@ -0,0 +1,6 @@
+/*
+you can execute this script with this commmand: 
+psql -U <username> -d <database> -f <file>
+ */
+
+INSERT INTO users (username, password) VALUES ('toto', '$2b$10$jU0lrBdzMF1p9iy.gz40s.D0PMX1PXfn77mgStpAzed8ygRHGsk/K');
