790e4cf48e7c60dc2c64681443b57430a8eecd36
Amaury <iamaury1380@gmail.com>
2023-10-18 23:38:03 +0200

    Added admin route

--- src/lib/server/account.js
+++ src/lib/server/account.js
@@ -118,23 +118,5 @@ export async function checkIfAdmin({ locals }) {
 		values: [locals.userInfo.user_id]
 	});
 
-	return rows[0].exists;
-}
-
-export async function checkIfPasswordIsCorrect(locals, username, password) {
-	const { rows } = await locals.pool.query({
-		text: 'SELECT password AS hash FROM users WHERE username = $1',
-		values: [username]
-	});
-
-	return await bcrypt.compare(password, rows[0].hash);
-}
-
-export async function getUserId(locals, username) {
-	const { rows } = await locals.pool.query({
-		text: 'SELECT user_id AS id FROM users WHERE username = $1',
-		values: [username]
-	});
-
-	return rows[0].id;
+	return rows[0] ? true : false;
 }
