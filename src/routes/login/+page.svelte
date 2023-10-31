6b29bbef2b934d8b16b2164deb33bedf1aa58149
Noé Libon <noe@libon.eu>
2023-10-31 20:41:32 +0100

    feat: frontend login form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,28 @@
-<h1>login page</h1>
+<script>
+    /** @type {import('./$types').ActionData} */
+    export let form;
+</script>
+
+<h1>/ Login /</h1>
+
+<form method="POST">
+    <label>
+        Username
+        <input type="text" name="username" placeholder="username" required>
+    </label>
+
+    <label>
+        Password
+        <input type="password" name="password" placeholder="password" required>
+    </label>
+
+    <button>Login</button>
+
+    {#if form?.success}
+        <p>{form.success}</p>
+    {/if}
+
+    {#if form?.error}
+        <p>{form.error}</p>
+    {/if}
+</form>
