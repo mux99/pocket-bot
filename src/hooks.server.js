48800cb997b07f524fa4c00a14eba13a1c36ce4f
Noé Libon <noe@libon.eu>
2023-11-02 18:06:47 +0100

    fix: redirect when logged in

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -11,12 +11,14 @@ export async function handle({ event, resolve }) {
 
 	if (event.url.pathname !== '/login' && event.url.pathname !== '/register') {
 		if (!event.locals.userInfo) throw redirect(308, '/login');
-	}
+	} else {
+		if (event.locals.userInfo) throw redirect(303, '/');
+	};
 
 	const response = await resolve(event);
 
 	return response;
-}
+};
 
 export function handleError({ event, error }) {
 	console.error(error.stack);
