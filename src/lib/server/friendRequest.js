dcc48def78155be0600b3e29bd4242de4bc3dd49
Noé Libon <noe@libon.eu>
2023-11-08 21:25:18 +0100

    refactor: del unnecessary functions

--- src/lib/server/friendRequest.js
+++ src/lib/server/friendRequest.js
@@ -4,59 +4,17 @@ export async function getFormData(request) {
     return { username };
 }
 
-export async function getReceiverId(locals, username) {
-    let errors = {};
-
-    const query = await locals.pool.query({
-        text: 'SELECT user_id FROM users WHERE username = $1',
-        values: [username]
-    });
-
-    if (query.rows.length > 0) {
-        return {
-            errors: errors,
-            receiverId: query.rows[0].user_id
-        };
-    }
-
-    errors.receiver = "Username does not exists"
-    return {
-        errors: errors,
-        receiverId: null
-    };
-}
-
-export async function getSenderId(cookies, locals) {
-    const uuid = cookies.get('uuid');
-    if (!uuid) {
-        return null;
-    }
-
-    const query = await locals.pool.query({
-        text: 'SELECT user_id FROM sessions WHERE uuid = $1',
-        values: [uuid]
-    });
-
-    return query.rows[0].user_id
-}
-
-export async function sendFriendRequest(locals, sender_id, receiver_id){
-    let errors = {};
+export async function sendFriendRequest(locals, senderId, receiverId){
     const date = new Date();
 
     try {
         await locals.pool.query({
             text: 'INSERT INTO friend_requests (sender_id, receiver_id, sent_on) VALUES ($1, $2, $3)',
-            values: [sender_id, receiver_id, date.toISOString()]
+            values: [senderId, receiverId, date.toISOString()]
         });
-        return errors;
-
     } catch (error) {
-        if (error.code === 23505) {
-            errors.request = "Request exists";
-            return errors;
-        }
+        return false;
+    };
 
-        throw Error(error);
-    }
+    return true;
 }
