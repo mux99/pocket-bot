d3de8a59c766af119611abbd2f444e6ea542c263
MachiganMC <xnagos@gmail.com>
2023-11-08 23:27:12 +0100

    feat(page): ajout page archives parties

--- src/lib/server/account.js
+++ src/lib/server/account.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import {pool} from "../../hooks.server.js";
 
 export async function getFormData(request) {
 	const formData = await request.formData();
@@ -108,7 +109,6 @@ export async function getUserinfo({ cookies, locals }) {
 		text: 'SELECT user_id, username FROM sessions JOIN users USING (user_id) WHERE uuid = $1 AND expires_at > NOW()',
 		values: [uuid]
 	});
-
 	return rows[0];
 }
 
@@ -137,4 +137,23 @@ export async function getUserId(locals, username) {
 	});
 
 	return rows[0].id;
+}
+
+export async function getArchiveParts(userId) {;
+	const {rows} = await pool.query({
+	text: "SELECT\n" +
+		"    part_id AS \"id\",\n" +
+		"    winner.user_id AS \"winner_id\",\n" +
+		"    winner.username AS \"winner_username\",\n" +
+		"    loser.user_id AS \"loser_id\",\n" +
+		"    loser.username AS \"loser_username\",\n" +
+		"    duration_ms AS \"duration\",\n" +
+		"    date\n" +
+		"FROM archive_parts AS parts\n" +
+		"JOIN users AS winner ON parts.winner = winner.user_id\n" +
+		"JOIN users AS loser ON parts.loser = loser.user_id\n" +
+		"WHERE winner=$1 OR loser=$1;",
+	values: [userId]
+	});
+	return rows;
 }
