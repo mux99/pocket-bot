430fe7f18a07597f3b98365d743d4fdcdf9e53f9
karitchi <karichikarichikarichi@gmail.com>
2023-11-20 10:52:12 +0100

    fix: Implement redirect logic for logged-in users
    This fix implements redirect logic to ensure that logged-in users are redirected to `/` if they attempt to access `/login` or `/register`.
    
    * Added redirect logic to check if a user is logged in and attempting to access `/login` or `/register`.
    * Redirects logged-in users to `/` if they attempt to access `/login` or `/register`.
    
    **Previous behavior:**
    
    * Logged-in users could access `/login` and `/register` without being redirected.

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -9,9 +9,15 @@ export async function handle({ event, resolve }) {
 	event.locals.pool = pool;
 	event.locals.userInfo = await getUserinfo(event);
 
-	// if user is not logged in, redirect him to /login
+
+	// if user is not logged in, redirect him to /login and allow him to navigate to /register
 	if (!event.locals.userInfo && event.url.pathname !== '/login' && event.url.pathname !== '/register') {
 		throw redirect(307, '/login');
+	}
+
+	// if user is logged in and navigate to /login or /register, redirect him to /
+	if (event.locals.userInfo && (event.url.pathname == '/login' || event.url.pathname == '/register')) {
+		throw redirect(307, '/');
 	}
 	const response = await resolve(event);
 
