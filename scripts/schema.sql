aa77eb575e292630aad19713708dee3ed28396de
Amaury <iamaury1380@gmail.com>
2023-12-05 17:04:19 +0100

    Update scripts/schema.sql
    Co-authored-by: MachiganMC <67206102+MachiganMC@users.noreply.github.com>

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS roles (
 CREATE TABLE IF NOT EXISTS users_roles (
   user_id INT NOT NULL,
   role_id INT NOT NULL,
+  CONSTRAINT pk_users_roles PRIMARY KEY (user_id, role_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
   FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE
 );
