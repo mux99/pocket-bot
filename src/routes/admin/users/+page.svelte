ebf40bfc36f099ef4015ef79c498328191b01e06
Amaury <iamaury1380@gmail.com>
2023-12-05 17:44:12 +0100

    feat: added css to the users page

--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -2,24 +2,51 @@
     export let data;
 </script>
 
-<h2>There are a total of { data.users.length } users registered.</h2>
+<style>
+    h2 {
+        color: #ddd; /* Light grey color for the heading */
+        font-size: 24px;
+    }
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    th, td {
+        border: 1px solid #888; /* Darker grey color for the border */
+        padding: 8px;
+        text-align: left;
+        color: #ddd; /* Light grey color for the text */
+    }
+    tr:nth-child(even) {
+        background-color: #333; /* Darker grey color for every other row */
+    }
+    th {
+        background-color: #555; /* Even darker grey color for the table headers */
+        color: #ddd; /* Light grey color for the header text */
+    }
+</style>
 
-<table>
-    <thead>
-        <tr>
-            <th>User ID</th>
-            <th>Username</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each data.users as user (user.user_id)}
+<div class="max-w-lg mx-auto overflow-x-auto">
+    <h2 class="text-lg text-white font-semibold mb-4 py-3">There are a total of <span class="bg-blue-500 text-white px-2 py-1 rounded">{ data.users.length }</span> users registered.</h2>
+    <table class="w-full">
+        <thead>
             <tr>
-                <td>{user.user_id}</td>
-                <td>{user.username}</td>
-                <td><a href="/admin/users/{user.user_id}">View</a></td>
-                <td><a href="/admin/users/{user.user_id}/edit">Edit</a></td>
-                <td><a href="/admin/users/{user.user_id}/delete">Delete</a></td>
+                <th>User ID</th>
+                <th>Username</th>
             </tr>
-        {/each}
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            {#each data.users as user (user.user_id)}
+                <tr>
+                    <td>{user.user_id}</td>
+                    <td>{user.username}</td>
+                    <td>
+                        <a href="/admin/users/{user.user_id}" class="px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-700">View</a>
+                        <a href="/admin/users/{user.user_id}/edit" class="px-2 py-1 bg-green-500 text-white rounded hover:bg-green-700 ml-2">Edit</a>
+                        <a href="/admin/users/{user.user_id}/delete" class="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-700 ml-2">Delete</a>
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
