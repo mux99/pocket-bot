f035679296bc2814962cc2672dcc8117841776a2
Amaury <iamaury1380@gmail.com>
2023-12-05 17:44:00 +0100

    feat: usage of the new roles system

--- src/routes/admin/users/[userId]/+page.server.js
+++ src/routes/admin/users/[userId]/+page.server.js
@@ -1,9 +1,15 @@
-import {checkIfAdmin} from "$lib/server/account.js";
+import {getUserRoles} from "$lib/server/account.js";
 import {redirect} from "@sveltejs/kit";
 
 export const load = async (serverLoadEvent) => {
-    if(!await checkIfAdmin(serverLoadEvent))
-        throw redirect(308, '/');
+    const {locals} = serverLoadEvent;
+    if (!locals.userInfo)
+        throw redirect(303, '/login');
+
+    const roles = await getUserRoles(locals.userInfo.user_id);
+
+    if (!roles.includes('admin'))
+        throw redirect(303, '/');
 
     const { rows } = await serverLoadEvent.locals.pool.query('SELECT * FROM users WHERE user_id = $1', [serverLoadEvent.params.userId]);
 
