b29823e0a48489c8d7d24ef7ff4d411800703468
Amaury <iamaury1380@gmail.com>
2023-12-07 09:57:58 +0100

    feat: better looking dashboard

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,7 @@
-<a href="/admin/users" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View Users</a>
+<div class="container mx-auto px-4 py-5">
+    <h1 class="text-4xl font-bold mb-4">Admin Dashboard</h1>
+    <div class="flex space-x-4">
+        <a href="/admin/users" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View Users</a>
+        <a href="/admin/maintenance" class="inline-block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Maintenance</a>
+    </div>
+</div>
