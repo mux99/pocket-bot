85c8689bf2059d6e5b01b591e98a0db27f9544f6
Amaury <iamaury1380@gmail.com>
2023-12-07 10:02:06 +0100

    feat: add view parts button

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,6 +2,7 @@
     <h1 class="text-4xl font-bold mb-4 text-white">Admin Dashboard</h1>
     <div class="flex space-x-4">
         <a href="/admin/users" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View Users</a>
+        <a href="/admin/parts" class="inline-block bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View Parts</a>
         <a href="/admin/maintenance" class="inline-block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Maintenance</a>
     </div>
 </div>
