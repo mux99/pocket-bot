a0b1d42359eca8aa43fb29336cd737b76a339d2c
Amaury <iamaury1380@gmail.com>
2023-12-07 09:59:16 +0100

    fix: admin dashboard text has to be white

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,5 +1,5 @@
 <div class="container mx-auto px-4 py-5">
-    <h1 class="text-4xl font-bold mb-4">Admin Dashboard</h1>
+    <h1 class="text-4xl font-bold mb-4 text-white">Admin Dashboard</h1>
     <div class="flex space-x-4">
         <a href="/admin/users" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">View Users</a>
         <a href="/admin/maintenance" class="inline-block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Maintenance</a>
