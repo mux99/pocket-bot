438fe37cef53e73d659be83ace4160e31cc8978a
julienmeire <julienmeire@gmail.com>
2023-12-20 22:23:37 +0100

    ajout test footer spec

--- services/web/svelte-kit/src/routes/+layout.svelte
+++ services/web/svelte-kit/src/routes/+layout.svelte
@@ -3,19 +3,6 @@
     import "../app.css";
     import Footer from "$lib/components/Footer.svelte";
     import { page } from "$app/stores"
-    function checkLoginCookie() {
-        const cookies = document.cookie.split(';');
-
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-
-            if (cookie.startsWith('uuid=')) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 
     function showFooter() {
         if ($page.url.pathname === '/login' || $page.url.pathname === '/register')
@@ -24,7 +11,6 @@
            return data.is_connected;
         return true;
     }
-    console.log(data.is_connected);
 </script>
 
 <slot />
