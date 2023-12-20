d5842830ee69c946924c5e6bc4885decb0a6e6bb
maxime <mux99@live.be>
2023-12-20 13:43:57 +0100

    apply global css to login page

--- services/web/svelte-kit/src/routes/login/+page.svelte
+++ services/web/svelte-kit/src/routes/login/+page.svelte
@@ -3,51 +3,66 @@
     export let form;
 </script>
 
-<div class="bg-gray-900">
-    <div class="h-screen flex flex-col items-start justify-center items-center">
-        <div class="flex flex-col">
-            <div class="mb-1">
-                <form method="POST" class="flex flex-col border border-gray-700 rounded bg-gray-800">
-                    <div>
-                        <h1 class="text-white text-3xl m-4 mb-3">Login</h1>
-                    </div>
-                    <div class="flex flex-col mx-4 mt-1 mb-1">
-                        <label>
-                            <div class="text-sm text-white font-bold">Username</div>
-                            <div>
-                                <input type="text" name="username" placeholder="username" required class="text-white bg-gray-900 border-2 border-gray-700 rounded p-1 px-2">
-                            </div>
-                        </label>
-                    </div>
+<style>
+    .body {
+        background-color: var(--background-3);
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        top: 0;
 
-                    <div class="flex flex-col mx-4 mt-1 mb-1">
-                        <label>
-                            <div class="text-sm text-white font-bold">Password</div>
-                            <div>
-                                <input type="password" name="password" placeholder="password" required class="text-white bg-gray-900 border-2 border-gray-700 rounded p-1 px-2">
-                            </div>
-                        </label>
-                    </div>
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    form {
+       width: fit-content;
+       height: fit-content;
+       align-self: center;
+       margin: 1em auto 0 auto;
+    }
+    h1 {
+        margin-top: 0;
+    }
+    form > button {
+        width: 100%;
+        margin-bottom: 0;
+    }
+    .red {color: var(--red);}
+    .green{color: var(--green);}
+    .btm_text{
+        width: 100%;
+        text-align: center;
+        font-size: small;
+    }
+</style>
 
-                    <div class="mx-4 mt-2">
-                        <button class="rounded-lg p-1 px-3 bg-green-600 text-sm text-white font-bold">Login</button>
-                    </div>
-
-                    <div class="m-4 mt-0">
-                        {#if form?.message}
-                            {#if form?.success}
-                                <p class="text-green-600">{form?.message}</p>
-                            {:else}
-                                <p class="text-red-600">{form?.message}</p>
-                            {/if}
-                        {/if}
-                    </div>
-                </form>
+<div class="body">
+    <div>
+        <form method="POST">
+            <h1>Login</h1>
+            <label>
+                <div>Username</div>
+                <input type="text" name="username" placeholder="username" required>
+            </label>
+            <label>
+                <div>Password</div>
+                <input type="password" name="password" placeholder="password" required>
+            </label><br>
+            <button class="big_button">Login</button>
+            <div>
+                {#if form?.message}
+                    {#if form?.success}
+                        <p class="green">{form?.message}</p>
+                    {:else}
+                        <p class="red">{form?.message}</p>
+                    {/if}
+                {/if}
             </div>
-            <div class="flex mt-1">
-                <div class="text-white text-xs mr-1">Don't have an account?</div>
-                <a href="/register" class="text-white hover:underline hover:text-blue-500 active:underline active:text-orange-500"><div class="text-xs ml-1">Register</div></a>
-            </div>
+        </form>
+        <div class="btm_text">
+            <span>Don't have an account?</span>
+            <a href="/register"><div>Register</div></a>
         </div>
     </div>
 </div>
